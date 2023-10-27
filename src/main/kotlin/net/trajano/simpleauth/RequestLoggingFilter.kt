851daffebc13b2e0601020076b8d2ea2c5a19959
Archimedes Trajano <developer@trajano.net>
2023-10-27 01:34:29 -0400

    dump headers

--- src/main/kotlin/net/trajano/simpleauth/RequestLoggingFilter.kt
+++ src/main/kotlin/net/trajano/simpleauth/RequestLoggingFilter.kt
@@ -3,20 +3,47 @@ package net.trajano.simpleauth
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
-import org.springframework.web.server.ServerWebExchange
+import org.springframework.http.HttpHeaders
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.web.server.WebFilter
-import reactor.core.publisher.Mono
 
 @Configuration
 class RequestHeaderLoggingFilter {
+
+    @Bean
+    fun userDetailsService() = MapReactiveUserDetailsService(
+        User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("user")
+            .roles("USER")
+            .build()
+    )
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http.addFilterBefore(
+             WebFilter { exchange, chain ->
+                val requestHeaders = exchange.request.headers
+                logHeaders(requestHeaders)
+                chain.filter(exchange)
+            }, SecurityWebFiltersOrder.HTTPS_REDIRECT
+        ).authorizeExchange { exchanges -> exchanges.anyExchange().authenticated() }
+            .httpBasic(withDefaults())
+            .formLogin(withDefaults())
+        return http.build()
+    }
+
     @Bean
     fun logRequestHeaders(): WebFilter {
         return WebFilter { exchange, chain ->
             val requestHeaders = exchange.request.headers
             logHeaders(requestHeaders)
             chain.filter(exchange)
-        }.apply {
-            setOrder(Ordered.HIGHEST_PRECEDENCE) // Set the order to be highest
         }
     }
 
