e87af8706a981b84be046e5cd4b1c1d98294a22b
Archimedes Trajano <developer@trajano.net>
2023-10-27 00:49:06 -0400

    Update RootController.kt

--- src/main/kotlin/net/trajano/simpleauth/RootController.kt
+++ src/main/kotlin/net/trajano/simpleauth/RootController.kt
@@ -3,7 +3,7 @@ package net.trajano.simpleauth
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
@@ -11,6 +11,10 @@ import reactor.core.publisher.Mono
 class RootController {
     @GetMapping("/")
     @ResponseStatus(code= HttpStatus.NO_CONTENT)
-    fun index() : Mono<ResponseEntity<Void>> = Mono.just(ResponseEntity.status(HttpStatus.NO_CONTENT).build())
+    fun index( @RequestHeader headers: Map<String, String> ) : Mono<ResponseEntity<Void>> {
+        print(headers)
+        return Mono.just(ResponseEntity.status(HttpStatus.NO_CONTENT).build())
+    }
+    // = Mono.just(ResponseEntity.status(HttpStatus.NO_CONTENT).build())
 
 }
