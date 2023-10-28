b934a2bfc8b768938e1d6fa068c29041d5f03da9
Archimedes Trajano <developer@trajano.net>
2023-10-28 03:51:46 -0400

    Added redis libraries and switched to Session

--- src/main/kotlin/net/trajano/simpleauth/RootController.kt
+++ src/main/kotlin/net/trajano/simpleauth/RootController.kt
@@ -4,17 +4,14 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @Controller
 class RootController {
     @GetMapping("/")
-    @ResponseStatus(code= HttpStatus.NO_CONTENT)
-    fun index( @RequestHeader headers: Map<String, String> ) : Mono<ResponseEntity<Void>> {
-        print(headers)
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    fun index(): Mono<ResponseEntity<Void>> {
         return Mono.just(ResponseEntity.status(HttpStatus.NO_CONTENT).build())
     }
 
