c2c2bffaaf093c75daf1de99826684f814c040f1
Archimedes Trajano <developer@trajano.net>
2023-11-04 23:25:25 -0400

    set ram percentage

--- src/main/kotlin/net/trajano/simpleauth/SimpleAuthenticationServerApplication.kt
+++ src/main/kotlin/net/trajano/simpleauth/SimpleAuthenticationServerApplication.kt
@@ -1,15 +1,13 @@
 package net.trajano.simpleauth
 
-import io.lettuce.core.tracing.Tracing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import reactor.core.publisher.Hooks
 
 @SpringBootApplication
 class SimpleAuthenticationServerApplication
 
 fun main(args: Array<String>) {
-	Hooks.enableAutomaticContextPropagation()
-	Tracing.disabled()
+//	Hooks.enableAutomaticContextPropagation()
+//	Tracing.disabled()
 	runApplication<SimpleAuthenticationServerApplication>(*args)
 }
