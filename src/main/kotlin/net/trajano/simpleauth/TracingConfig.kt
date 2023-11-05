aa682536aa347c1e6e4998ab291cb05f4d4767e9
Archimedes Trajano <developer@trajano.net>
2023-11-04 22:28:40 -0400

    set ram percentage

--- src/main/kotlin/net/trajano/simpleauth/TracingConfig.kt
+++ src/main/kotlin/net/trajano/simpleauth/TracingConfig.kt
@@ -1,11 +1,13 @@
 package net.trajano.simpleauth
 
-import io.lettuce.core.resource.DefaultClientResources
+import io.lettuce.core.tracing.Tracing
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
 
 @Configuration
 class TracingConfig {
     @Bean
-    fun foo(d: DefaultClientResources) = ForwardHeadersRedirectStrategy()
+    @Lazy
+    fun foo(d: Tracing) = ForwardHeadersRedirectStrategy()
 }
