26ffb5561ba03faebb57f01532f33153893dc4e0
Archimedes Trajano <developer@trajano.net>
2024-05-21 15:45:12 -0400

    fixes

--- src/main/kotlin/net/trajano/simpleauth/ForwardHeadersRedirectStrategy.kt
+++ src/main/kotlin/net/trajano/simpleauth/ForwardHeadersRedirectStrategy.kt
@@ -22,20 +22,16 @@ class ForwardHeadersRedirectStrategy : ServerRedirectStrategy {
         if (url.startsWith("/") && exchange.request.headers["x-forwarded-proto"] != null) {
             val context = exchange.request.path.contextPath().value()
             val forwardedProto = exchange.request.headers["x-forwarded-proto"]!![0]
-            var forwardedPort : String = null
+            var forwardedPort : String? = null
             if (exchange.request.headers["x-forwarded-port"] != null) {
                 forwardedPort = exchange.request.headers["x-forwarded-port"]!![0]
             }
             when {
-                forwardedPort == null -> {
-                    forwardedPort = null;
-                }
-                
-                "https".equals(forwardedProto) && "443".equals(forwardedPort) -> {
+                "https" == forwardedProto && "443" == forwardedPort -> {
                     forwardedPort = null;
                 }
 
-                "http".equals(forwardedProto) && "80".equals(forwardedPort) -> {
+                "http" == forwardedProto && "80" == forwardedPort -> {
                     forwardedPort = null;
                 }
             }
